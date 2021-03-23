1e778ad76ab02dd55f1eefe9f0fb5c90113c402d
harryghgim <harryghgim@gmail.com>
2021-03-23 12:29:35 +0900

    model created and applied & settings.py

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    """
+    model for tasks to do 
+    """
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    completed_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
